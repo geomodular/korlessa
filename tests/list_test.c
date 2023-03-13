f9d5f62aa515adcd5ad468d5ce22573cea8601b7
Peter <geomodular@gmail.com>
2023-03-13 21:04:52 +0100

    Add remaining list tests

--- tests/list_test.c
+++ tests/list_test.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -101,6 +102,11 @@ void test_insert_after_first_element(struct test *t) {
     if (size != 6)
         failf(t, "expected size 6 got %d", size);
 
+    if (p == NULL) {
+        fail(t, "p should not be NULL");
+        return;
+    }
+
     if (strcmp("Margita", p->name) != 0)
         failf(t, "expected name Margita got %s", p->name);
 }
@@ -115,6 +121,11 @@ void test_insert_after_last_element(struct test *t) {
     if (size != 6)
         failf(t, "expected size 6 got %d", size);
 
+    if (p == NULL) {
+        fail(t, "p should not be NULL");
+        return;
+    }
+
     if (strcmp("Melania", p->name) != 0)
         failf(t, "expected name Melania got %s", p->name);
 }
@@ -126,23 +137,119 @@ void test_insert_into_null_list(struct test *t) {
         fail(t, "next item should stay NULL");
 }
 
+bool check_name(void *person, void *name) {
+    Person *p = person;
+    char *n = name;
+    return strcmp(p->name, n) == 0;
+}
+
+void test_find(struct test *t) {
+    Person *ps = get_static_list();
+    Person *p = list_find(ps, check_name, "Mima");
+
+    if (p == NULL) {
+        fail(t, "p should not be NULL");
+        return;
+    }
+
+    if (strcmp(p->name, "Mima") != 0)
+        failf(t, "expected name Mima got %s", p->name);
+}
+
+void test_find_phony(struct test *t) {
+    Person *ps = get_static_list();
+    Person *p = list_find(ps, check_name, "Matilda");
+
+    if (p != NULL)
+        fail(t, "p should be NULL");
+}
+
+void change_name(void *person) {
+    static char *c = "Name";
+    Person *p = person;
+    p->name = c;
+}
+
+void test_apply(struct test *t) {
+    Person *ps = get_static_list();
+    list_apply(ps, change_name);
+
+    for (Person *p = ps; p != NULL; p = p->l.next) {
+        if (strcmp(p->name, "Name") != 0)
+            failf(t, "expected name Name got %s", p->name);
+    }
+}
+
+void test_drop_apply(struct test *t) {
+    Person *ps = get_static_list();
+    Person *last = list_goto_last(ps);
+    Person *p = list_drop_apply(ps, change_name);
+    
+    int size = list_size(p);
+    if (size != 4)
+        failf(t, "expected size 4 got %d", size);
+
+    if (strcmp(last->name, "Name") != 0)
+        failf(t, "expected name Name got %s", p->name);
+}
+
+void test_drop_apply_null_list(struct test *t) {
+    Person *p = list_drop_apply(NULL, change_name);
+    if (p != NULL)
+        fail(t, "p should be NULL");
+}
+
+bool compare_name(void *person1, void *person2) {
+    Person *p1 = person1;
+    Person *p2 = person2;
+    size_t i;
+
+    for (i = 0; p1->name[i] == p2->name[i]; i++) {
+        if (p1->name[i] == '\0')
+            return true;
+    }
+
+    if ((unsigned char) p1->name[i] < (unsigned char) p2->name[i])
+        return false;
+
+    return true;
+}
+
+void test_sort(struct test *t) {
+    char *sorted[] = {"Maria", "Marta", "Martina", "Mima", "Monika"};
+
+    Person *ps = get_static_list();
+
+    size_t i = 0;
+    for (Person *p = list_sort(ps, compare_name); p != NULL; p = p->l.next, i++) {
+    if (strcmp(p->name, sorted[i]) != 0)
+        failf(t, "expected %d. entry Maria got %s", i+1, p->name);
+    }
+}
+
 int main(int argc, char **argv) {
 
     static test_fn tests[] = {
-        test_initialize, 
+        test_initialize,
         test_append,
         test_size_of_full_list, 
         test_size_of_one_element, 
         test_size_of_null,
         test_go_to_first_element,
         test_go_to_index_3,
-        test_go_to_last_element, 
+        test_go_to_last_element,
         test_go_to_non_existing_index,
-        test_go_over_null_list, 
+        test_go_over_null_list,
         test_go_to_last_element_over_null_list, 
-        test_insert_after_first_element, 
-        test_insert_after_last_element, 
-        test_insert_into_null_list, 
+        test_insert_after_first_element,
+        test_insert_after_last_element,
+        test_insert_into_null_list,
+        test_find,
+        test_find_phony,
+        test_apply,
+        test_drop_apply,
+        test_drop_apply_null_list,
+        test_sort,
         NULL,
     };
 
